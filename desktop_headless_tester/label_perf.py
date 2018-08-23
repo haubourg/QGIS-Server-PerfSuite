6393a5ee05c6e880386fe34111701ec725d1c4da
Régis Haubourg <regis.haubourg@oslandia.com>
2018-08-23 18:00:13 +0200

    1st working version

--- desktop_headless_tester/label_perf.py
+++ desktop_headless_tester/label_perf.py
@@ -1,3 +1,8 @@
+# this scripts needs to be run with the following environment variable set correctly with the prefix location
+# Ex:
+#   export LD_LIBRARY_PATH=/home/regis/APPS/QGIS3/qgis3_release/lib
+#   export LD_LIBRARY_PATH=/home/regis/APPS/QGIS3/qgis3_release/lib
+
 import time
 import sys
 import future
@@ -48,7 +53,8 @@ else :
     print('unsupported version. Exiting perf testing')
     exit()
 
-prefix = "/home/regis/APPS/QGIS3/qgis3_release/bin"
+prefix = "/usr"
+# prefix = "/home/regis/APPS/QGIS3/qgis3_release/"
 app = QApplication([])
 QgsApplication.setPrefixPath(prefix, True)
 QgsApplication.initQgis()
@@ -56,32 +62,42 @@ QgsApplication.initQgis()
 
 # init vector layer
 vl = QgsVectorLayer('/data/REFS/opendata/roads_osm.gpkg|roads_osm', 'roads_osm2', 'ogr')
+# vl = QgsVectorLayer('/home/regis/APPS/QGIS3/cpp/QGIS/tests/testdata/curved_polys.gpkg|layername=polys', 'curved_poly', 'ogr')
 
-if not vl :
+if vl == False :
     print('Error adding vector layer')
 
 print( str(vl.name() ) )
+# n_feat = vl.featureCount()
+# print( str( n_feat ) )
+# Ex:
+#   export LD_LIBRARY_PATH=/home/regis/APPS/QGIS3/qgis3_release/lib
+#   export
 project = QgsProject.instance()
 project.addMapLayers([vl])
+
+print('Project Layers:')
+for l in project.mapLayers() :
+    print(l)
 
 extent = vl.extent()
 
 # map settings
 size = QSize(1629, 800)
 ms = QgsMapSettings()
+
 crs = QgsCoordinateReferenceSystem("EPSG:2154")
 # ms.setExtent( QgsRectangle(-4.7785446167610397,48.2772163121766340,-4.5545540303610084,48.4047377555436498) )
 
 ms.setExtent( extent )
 ms.setOutputSize( size )
 
-
-
 # QGIS 2 specific
 
 if version < 3 :
 
     QgsMapLayerRegistry.instance().addMapLayer(vl, False)
+
 
 # QGIS 3 specific
 
@@ -91,9 +107,9 @@ if version >= 3 :
     canvas = QgsMapCanvas()
     canvas.setDestinationCrs(crs)
     canvas.setLayers([vl])
-    print(canvas.layers())
-    canvas.refresh()
-
+    # print(canvas.layers())
+    # canvas.show()
+    #canvas.refresh()
 
 
 
@@ -112,6 +128,7 @@ if version >= 3 :
 # place = QgsPalLayerSettings.Line
 # vl.setCustomProperty("labeling/placement", str(place))
 # vl.setCustomProperty("labeling/placementFlags", "14")
+ms.setLayers([vl])
 
 # ms.setLayers([vl])
 i0 = QImage(size, QImage.Format_RGB32)
@@ -121,22 +138,27 @@ j0 = QgsMapRendererCustomPainterJob(ms, p0)
 
 
 start = time.time()
-j0.renderSynchronously()
+j0.renderSynchronously()# Ex:
+#   export LD_LIBRARY_PATH=/home/regis/APPS/QGIS3/qgis3_release/lib
+#   export
 t0 = time.time() - start
 
 p0.end()
+i0.save('/tmp/para.png')
 
 # horizontal
 # place = QgsPalLayerSettings.Horizontal
 # vl.setCustomProperty("labeling/placement", str(place))
+
 # vl.setCustomProperty("labeling/placementFlags", "14")
 
-ms.setLayers([vl])
 i1 = QImage(size, QImage.Format_RGB32)
-# i1.fill(Qt.white)
+i1.fill(Qt.white)
 p1 = QPainter(i1)
 j1 = QgsMapRendererCustomPainterJob(ms, p1)
 start = time.time()
+
+
 j1.renderSynchronously()
 t1 = time.time() - start
 
@@ -146,5 +168,4 @@ i1.save('/tmp/hori.png')
 print(str('PERF_PARA: ') + str(t0))
 print(str('PERF_HORI: ') + str(t1))
 print(str('PERF_RATIO: ') + str(t0/t1))
-
-app.exit()
+# app.exit()
